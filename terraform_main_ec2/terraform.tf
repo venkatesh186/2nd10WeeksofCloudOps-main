07567c67be9add7f737099e889ce5b592448128a
venkatesh186 <arikicherlavenkatesulu@gmail.com>
2024-11-28 16:05:32 +0530

    Update terraform.tf

--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -9,11 +9,11 @@ terraform {
   backend "s3" {
     bucket = "nareshawsdevv"
     key    = "ec2/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 
   required_version = ">= 1.6.3"
 }
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
